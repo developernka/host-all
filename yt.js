656a62a9c9dc783e427a205dab17c875f3134439
developernka <developer.nka@gmail.com>
2019-08-31 22:14:28 +0530

    Update yt.js

--- yt.js
+++ yt.js
@@ -1,1 +1,2 @@
 sessionStorage['yt-player-playback-rate'] = JSON.stringify({"data":"1.35","creation":new Date().getTime()});
+localStorage['yt-player-quality'] = JSON.stringify({"data":"medium","expiration":new Date().getTime()+24*3600*1000,"creation":new Date().getTime()});
